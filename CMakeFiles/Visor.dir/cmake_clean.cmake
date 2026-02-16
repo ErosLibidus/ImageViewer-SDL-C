26967fffdf07bfc8dd3fe36aacbd74a56916b63c
Tu Nombre <you@example.com>
2026-02-16 18:04:25 -0300

    CMake

---
+++ CMakeFiles/Visor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Visor.dir/link.d"
+  "CMakeFiles/Visor.dir/src/main.c.o"
+  "CMakeFiles/Visor.dir/src/main.c.o.d"
+  "Visor"
+  "Visor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Visor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/Visor.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Visor.
+# This may be replaced when dependencies are built.
